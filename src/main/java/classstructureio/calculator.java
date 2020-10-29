75886d63ec88a1b6932a3dc98af47975550a94f5
FHNora <horvath.nora92@gmail.com>
2020-10-29 15:54:17 +0100

    Initial commit

---
+++ src/main/java/classstructureio/calculator.java
@@ -0,0 +1,15 @@
+package classstructureio;
+
+import java.util.Scanner;
+
+public class calculator {
+
+    public static void main(String[] args) {
+
+        System.out.println("Írjon be két számot!");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        System.out.println(num1 + num2);
+    }
+}
