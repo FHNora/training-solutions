a26f7f4e32cf24e994590a6f5fac55e4ce89d945
FHNora <horvath.nora92@gmail.com>
2020-11-05 14:19:43 +0100

    class Registration added

--- src/main/java/stringtype/StringTypeMain.java
+++ src/main/java/stringtype/StringTypeMain.java
@@ -1,4 +1,25 @@
 package stringtype;
 
 public class StringTypeMain {
+    public static void main(String[] args) {
+        String prefix = "Hello ";
+        String name = "John Doe";
+        String message = prefix + name;
+        int a = 444;
+        message = message + a;
+        System.out.println(message);
+        boolean b = message.equals("Hello John Doe");
+        System.out.println(b);
+        boolean c = message.equals("Hello John Doe444");
+        System.out.println(c);
+        String k = "";
+        String l = "";
+        String m = k + l;
+        System.out.println(m);
+        System.out.println(m.length());
+        String z = "Abcde";
+        System.out.println(z.length());
+        System.out.println(z.substring(0, 1) + "," + z.substring(2,3));
+
+    }
 }
--- src/main/java/stringtype/UserValidator.java
+++ src/main/java/stringtype/UserValidator.java
@@ -1,4 +1,13 @@
 package stringtype;
 
 public class UserValidator {
+    String username;
+    String password;
+    String email;
+
+    public boolean isValidUsername(String username){
+        if (username != null){
+
+        }
+    }
 }
