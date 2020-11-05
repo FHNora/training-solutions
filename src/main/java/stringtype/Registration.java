3cc2c2a379447e85978bc66bb1fffc3ad75c4252
FHNora <horvath.nora92@gmail.com>
2020-11-05 15:33:40 +0100

    class Registration modified

--- src/main/java/stringtype/Registration.java
+++ src/main/java/stringtype/Registration.java
@@ -1,4 +1,19 @@
 package stringtype;
+import java.util.Scanner;
 
 public class Registration {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Registration registration = new Registration();
+        System.out.println("Kérem adja meg a felhasználónevét!");
+        String username = scanner.nextLine();
+        System.out.println(username);
+
+        System.out.println("Kérem adja meg a jelszavát kétszer!");
+        String password1 = scanner.nextLine();
+        System.out.println(password1);
+
+        String password2 = scanner.nextLine();
+        System.out.println(password2);
+    }
 }
--- src/main/java/stringtype/UserValidator.java
+++ src/main/java/stringtype/UserValidator.java
@@ -1,13 +1,28 @@
 package stringtype;
 
 public class UserValidator {
-    String username;
-    String password;
-    String email;
 
-    public boolean isValidUsername(String username){
-        if (username != null){
-
+    public boolean isValidUsername(String username) {
+        if (username == null) {
+            return false;
         }
+        return true;
+    }
+
+    public boolean isValidPassword(String password1, String password2) {
+        if (password1.length() < 8) {
+            return false;
+        }
+        if (password2.length() < 8) {
+            return false;
+        }
+        if (password1.equals(password2)) {
+            return true;
+        }
+        return true;
+    }
+
+    public boolean isValidEmail(String email){
+        
     }
 }
