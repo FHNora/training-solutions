fe569b7717ef9f3b0c1aba5bc7317dc3ba65c332
FHNora <horvath.nora92@gmail.com>
2020-11-06 14:48:21 +0100

    class PerformanceTest added

--- src/main/java/introdate/Performance.java
+++ src/main/java/introdate/Performance.java
@@ -1,4 +1,27 @@
 package introdate;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class Performance {
+    private LocalDate date;
+    private String artist;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
 }
