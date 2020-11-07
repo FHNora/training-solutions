2e71493bd6afd1624206bf6d96a126db77b3e4d4
FHNora <horvath.nora92@gmail.com>
2020-11-07 22:49:50 +0100

    class BoatRental added

--- src/main/java/introcontrol/Sum.java
+++ src/main/java/introcontrol/Sum.java
@@ -1,5 +1,16 @@
 package introcontrol;
 
+import java.util.Scanner;
+
 public class Sum {
-    
+    public static void main(String[] args) {
+        System.out.println("Kérem adjon meg 5 számot!");
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        int num4 = scanner.nextInt();
+        int num5 = scanner.nextInt();
+        System.out.println(num1 + num2 + num3 + num4 + num5);
+    }
 }
