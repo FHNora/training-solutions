45bcf402165e961bfcc68766127126b7523091db
FHNora <horvath.nora92@gmail.com>
2020-11-07 21:58:50 +0100

    class Qualifier added

--- src/main/java/introcontrol/IntroControl.java
+++ src/main/java/introcontrol/IntroControl.java
@@ -1,58 +1,63 @@
 package introcontrol;
 
 public class IntroControl {
-    public int substractTenIfGreaterThenTen(int number){
-        if (number <= 10){
+    public int substractTenIfGreaterThenTen(int number) {
+        if (number <= 10) {
             return number;
-        }
-        else return number - 10;
+        } else return number - 10;
     }
 
-    public String describeNumber(int number){
-        if (number == 0){
+    public String describeNumber(int number) {
+        if (number == 0) {
             return "zero";
-        }
-        else return "not zero";
+        } else return "not zero";
     }
 
-    public String greetingJoe(String name){
-        if (name.equals("Joe")){
+    public String greetingJoe(String name) {
+        if (name.equals("Joe")) {
             return "Hello Joe";
-        }
-        else return "";
+        } else return "";
     }
 
-    public int calculateBonus(int sale){
-        if (sale > 1_000_000){
+    public int calculateBonus(int sale) {
+        if (sale > 1_000_000) {
             return (sale / 100) * 10;
-        }
-        else return 0;
+        } else return 0;
     }
 
-   public void printNumbers(int max){
-        for (int i = 0; i <= max; i++){
+    public void printNumbers(int max) {
+        for (int i = 0; i <= max; i++) {
             System.out.println(i);
         }
-   }
+    }
 
-   public void printNumbersBetween(int min, int max){
-        for (int i = 0; i <=max; i++){
-            if (i >= min && i <= max){
+    public void printNumbersBetween(int min, int max) {
+        for (int i = 0; i <= max; i++) {
+            if (i >= min && i <= max) {
                 System.out.println(i);
             }
         }
-   }
+    }
 
-   public void printNumbersBetweenAnyDirection(int a, int b){
-        for (int i = 0; i < 100; i++){
-            if (a > b) {
+    public void printNumbersBetweenAnyDirection(int a, int b){
+    if (a > b){
+        for (int i = a; i >= b; i--) {
+            System.out.println(i);
+        }
+    } else {
+        for (int i = a; i <= b; i++){
+            System.out.println(i);
+        }
+    }
+    }
+
+    public void printOddNumbers(int max){
+        for (int i = 1; i <= max; i++){
+            if (i % 2 == 1){
                 System.out.println(i);
             }
         }
-        for (int i = 0; i <100; i--){
-            if (b > a){
-                System.out.println(i);
-            }
-        }
-   }
+    }
 }
+
+
--- src/main/java/introcontrol/IntroControlMain.java
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -7,7 +7,15 @@ public class IntroControlMain {
         System.out.println(introControl.describeNumber(0));
         System.out.println(introControl.greetingJoe("Jane"));
         System.out.println(introControl.calculateBonus(2000000));
+        System.out.println(" ");
         introControl.printNumbers(5);
+        System.out.println(" ");
         introControl.printNumbersBetween(3,6);
+        System.out.println(" ");
+        introControl.printNumbersBetweenAnyDirection(4, 8);
+        System.out.println(" ");
+        introControl.printNumbersBetweenAnyDirection(4, 1);
+        System.out.println(" ");
+        introControl.printOddNumbers(17);
     }
 }
