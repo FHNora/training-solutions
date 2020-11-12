427e1c0786f9a230532616fca6d6fab21cffe0f2
FHNora <horvath.nora92@gmail.com>
2020-11-12 21:28:59 +0100

    class ArrayHandler added

--- src/main/java/arrays/ArrayMain.java
+++ src/main/java/arrays/ArrayMain.java
@@ -6,11 +6,6 @@ public class ArrayMain {
         System.out.println(days[1]);
         System.out.println(days.length);
 
-        int[] involve = new int[5];
-        for (int i = 0; i < involve.length; i++){
-            System.out.println(2*i);
-        }
-
         boolean[] trueOrFalse = {false, false, true, true, true, false};
         for (int i = 0; i < trueOrFalse.length; i++){
             System.out.println(trueOrFalse[i]);
