404fad7e96222efa52516a3ff02d2835af8a8eac
FHNora <horvath.nora92@gmail.com>
2020-11-12 15:42:12 +0100

    class ArrayMain modified

--- src/main/java/arrays/ArrayMain.java
+++ src/main/java/arrays/ArrayMain.java
@@ -5,5 +5,15 @@ public class ArrayMain {
         String[] days = {"hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap"};
         System.out.println(days[1]);
         System.out.println(days.length);
+
+        int[] involve = new int[5];
+        for (int i = 0; i < involve.length; i++){
+            System.out.println(2*i);
+        }
+
+        boolean[] trueOrFalse = {false, false, true, true, true, false};
+        for (int i = 0; i < trueOrFalse.length; i++){
+            System.out.println(trueOrFalse[i]);
+        }
     }
 }
