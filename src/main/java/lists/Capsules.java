df2b3e5f5109f451c4fdbb367e95071a3d54ba8b
FHNora <horvath.nora92@gmail.com>
2020-11-14 14:20:36 +0100

    class CircleCalculator added

---
+++ src/main/java/lists/Capsules.java
@@ -0,0 +1,16 @@
+package lists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capsules {
+
+    private ArrayList<String> colours;
+
+    List<String> capsules = new ArrayList<>();
+
+    public void addLast(String colours){
+
+
+    }
+}
