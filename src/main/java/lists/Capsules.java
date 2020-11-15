17512ec4c8d14d7d032088a01c4d065471432185
FHNora <horvath.nora92@gmail.com>
2020-11-15 21:53:26 +0100

    class ObjectsMain added

--- src/main/java/lists/Capsules.java
+++ src/main/java/lists/Capsules.java
@@ -27,21 +27,4 @@ public class Capsules {
         return colours;
     }
 
-    public static void main(String[] args) {
-       Capsules capsules = new Capsules();
-       capsules.addFirst("red");
-        System.out.println(capsules.getColors());
-
-        capsules.addFirst("green");
-        System.out.println(capsules.getColors());
-
-        capsules.addLast("yellow");
-        System.out.println(capsules.getColors());
-
-        capsules.removeFirst();
-        System.out.println(capsules.getColors());
-
-        capsules.removeLast();
-        System.out.println(capsules.getColors());
-    }
 }
--- src/main/java/lists/CapsulesMain.java
+++ src/main/java/lists/CapsulesMain.java
@@ -1,4 +1,21 @@
 package lists;
 
 public class CapsulesMain {
+    public static void main(String[] args) {
+        Capsules capsules = new Capsules();
+        capsules.addFirst("red");
+        System.out.println(capsules.getColors());
+
+        capsules.addFirst("green");
+        System.out.println(capsules.getColors());
+
+        capsules.addLast("yellow");
+        System.out.println(capsules.getColors());
+
+        capsules.removeFirst();
+        System.out.println(capsules.getColors());
+
+        capsules.removeLast();
+        System.out.println(capsules.getColors());
+    }
 }
