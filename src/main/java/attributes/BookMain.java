f9206d9d04d60c85236369c7b78dcb3bcee047a1
FHNora <horvath.nora92@gmail.com>
2020-11-16 15:26:01 +0100

    class Task added

--- src/main/java/attributes/BookMain.java
+++ src/main/java/attributes/BookMain.java
@@ -1,4 +1,9 @@
 package attributes;
 
 public class BookMain {
+    public static void main(String[] args) {
+        Book book = new Book("A kis herceg");
+        book.setTitle("Vuk");
+        System.out.println(book.getTitle());
+    }
 }
