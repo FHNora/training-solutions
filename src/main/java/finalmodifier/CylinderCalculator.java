ff669092ad28b335b9a3ff1a880f7339e9c59086
FHNora <horvath.nora92@gmail.com>
2020-11-16 11:22:20 +0100

    class CylinderCalculator modified

--- src/main/java/finalmodifier/CylinderCalculator.java
+++ src/main/java/finalmodifier/CylinderCalculator.java
@@ -1,4 +1,15 @@
 package finalmodifier;
 
 public class CylinderCalculator {
+
+    public CylinderCalculator(double r){
+        this.r = r;
+    }
+    public double calculateVolume(double r, int h){
+        return CircleCalculator.PI * r * r * h;
+    }
+
+    public double calculateSurfaceArea(double r, int h){
+        return CircleCalculator.PI * 2 * r * (r + h);
+    }
 }
--- src/main/java/finalmodifier/PiMain.java
+++ src/main/java/finalmodifier/PiMain.java
@@ -3,6 +3,10 @@ package finalmodifier;
 public class PiMain {
     public static void main(String[] args) {
         CircleCalculator circleCalculator = new CircleCalculator(3);
+        CylinderCalculator cylinderCalculator = new CylinderCalculator(3);
+        System.out.println(CircleCalculator.PI);
         System.out.println(circleCalculator.calculatePerimeter(2.5));
+        System.out.println(cylinderCalculator.calculateVolume());
+
     }
 }
