6adda568131f34cf2ff87ca1392a16354ed5fc7e
FHNora <horvath.nora92@gmail.com>
2020-11-16 15:29:44 +0100

    class Task modified

--- src/main/java/constructors/Task.java
+++ src/main/java/constructors/Task.java
@@ -1,4 +1,19 @@
 package constructors;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Task {
+    String title;
+    String description;
+    LocalDateTime startDateTime;
+    LocalTime duration;
+
+    public Task(String title, String description, LocalDateTime startDateTime, LocalTime duration ){
+        this.title = title;
+        this.description = description;
+        this.startDateTime = startDateTime;
+        this.duration = duration;
+    }
+    
 }
