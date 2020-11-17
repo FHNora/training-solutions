f9ec0d6eaf8f05ec84de20d7d8ebede9a3da54d2
FHNora <horvath.nora92@gmail.com>
2020-11-17 15:31:19 +0100

    class ReferencesMain modified

--- src/main/java/references/ReferencesMain.java
+++ src/main/java/references/ReferencesMain.java
@@ -1,4 +1,11 @@
 package references;
 
 public class ReferencesMain {
+    public static void main(String[] args) {
+     Person firstPerson;
+     Person secondPerson;
+     Person thirdPerson = new Person("John Doe", 47);
+     firstPerson = thirdPerson;
+
+    }
 }
