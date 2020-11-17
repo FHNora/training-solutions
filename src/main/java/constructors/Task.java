2ed0ed423d051f23ac7481155f50b21d803e15fa
FHNora <horvath.nora92@gmail.com>
2020-11-17 14:18:51 +0100

    class TaskMain added

--- src/main/java/constructors/Task.java
+++ src/main/java/constructors/Task.java
@@ -9,11 +9,32 @@ public class Task {
     LocalDateTime startDateTime;
     LocalTime duration;
 
-    public Task(String title, String description, LocalDateTime startDateTime, LocalTime duration ){
+    public Task(String title, String description){
         this.title = title;
         this.description = description;
-        this.startDateTime = startDateTime;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getStartDateTime(){
+        return startDateTime;
+    }
+
+    public LocalTime getDuration() {
+        return duration;
+    }
+
+    public void setDuration(LocalTime duration) {
         this.duration = duration;
     }
-    
+
+    public LocalDateTime start(LocalDateTime startDateTime){
+        this.startDateTime = LocalDateTime.now();
+    }
 }
