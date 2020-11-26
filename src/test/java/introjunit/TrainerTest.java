dd0e5c93eb5de4f86e987c7c240b5fe8197df523
FHNora <horvath.nora92@gmail.com>
2020-11-26 14:11:09 +0100

    class Greetings added

--- src/test/java/introjunit/TrainerTest.java
+++ src/test/java/introjunit/TrainerTest.java
@@ -1,4 +1,17 @@
 package introjunit;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TrainerTest {
+
+    @Test
+    void testCreate(){
+        Trainer trainer = new Trainer("John Doe");
+
+        String name = trainer.getName();
+
+        assertEquals("John Doe", name);
+    }
 }
