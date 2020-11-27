5859043abc6b94adddf2ce103cbd5ab7ca57bcea
FHNora <horvath.nora92@gmail.com>
2020-11-27 15:11:59 +0100

    class Dictionary added

---
+++ src/main/java/contolliteration/Dictionary.java
@@ -0,0 +1,4 @@
+package contolliteration;
+
+public class Dictionary {
+}
--- src/main/java/contolliteration/DictionaryItem.java
+++ src/main/java/contolliteration/DictionaryItem.java
@@ -1,4 +1,25 @@
 package contolliteration;
 
+import java.util.List;
+
 public class DictionaryItem {
+    private String word;
+    private List<String> translations;
+
+    public DictionaryItem(String word, List<String> translations>){
+        this.word = word;
+        this.translations = addTranslations();
+    }
+
+    public List<String> addTranslations(String word, List<String> translations){
+
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public List<String> getTranslations() {
+        return translations;
+    }
 }
