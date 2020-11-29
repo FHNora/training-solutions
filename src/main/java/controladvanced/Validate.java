8094e9994805cf8d64d23858df2844ba437197d9
FHNora <horvath.nora92@gmail.com>
2020-11-29 22:14:27 +0100

    class Validate modified

--- src/main/java/controladvanced/Validate.java
+++ src/main/java/controladvanced/Validate.java
@@ -1,4 +1,17 @@
 package controladvanced;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Validate {
+
+    public List<String> filterLines(List<String> lines) {
+        List<String> validLines = new ArrayList<>();
+        for (String line : lines) {
+            String[] parts = line.split(";");
+            if(parts.length != 4){
+                return null;
+            }
+        }
+    }
 }
