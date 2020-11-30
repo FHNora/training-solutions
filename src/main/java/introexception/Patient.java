ea27fd19d0397937efcb66e2ea43b199056793ac
FHNora <horvath.nora92@gmail.com>
2020-11-30 21:36:32 +0100

    class SsnValidator added

--- src/main/java/introexception/Patient.java
+++ src/main/java/introexception/Patient.java
@@ -1,4 +1,32 @@
 package introexception;
 
 public class Patient {
+    private String name;
+    private String socialSecurityNumber;
+    private int yearOfBirth;
+
+    public Patient (String name, String socialSecurityNumber, int yearOfBirth){
+
+        if (name == ""){
+          throw new IllegalArgumentException("Name is not given");
+        }
+        if (yearOfBirth < 1900){
+            throw new IllegalArgumentException("Year of Birth should be more than 1990");
+        }
+        this.name = name;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public int getYearOfBirth(){
+        return yearOfBirth;
+    }
 }
