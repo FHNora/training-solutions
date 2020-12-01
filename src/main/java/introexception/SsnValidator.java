8eb66936fc26981c97c824c0f1fe00bb5f173e8d
FHNora <horvath.nora92@gmail.com>
2020-12-01 15:52:41 +0100

    class SsnValidator added

--- src/main/java/introexception/SsnValidator.java
+++ src/main/java/introexception/SsnValidator.java
@@ -2,6 +2,8 @@ package introexception;
 
 public class SsnValidator {
     public boolean isValidSsn(String ssn){
-        if( )
+        if(ssn.charAt(8) != ){
+            throw new IllegalArgumentException("");
+        }
     }
 }
