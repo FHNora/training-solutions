0c4b30bb2e4df3fb6e4bf710852e28e06b4f2a14
FHNora <horvath.nora92@gmail.com>
2020-12-02 21:39:19 +0100

    class Vet added

--- src/main/java/stringbasic/Pet.java
+++ src/main/java/stringbasic/Pet.java
@@ -1,4 +1,26 @@
 package stringbasic;
 
 public class Pet {
+
+    private String name;
+    private int yearOfBirth;
+    private String gender;
+    private String registrationNumber;
+
+    public String getName(){
+        return name;
+    }
+
+    public int getYearOfBirth(){
+        return yearOfBirth;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+    public String getRegistrationNumber(){
+        return registrationNumber;
+    }
+
 }
