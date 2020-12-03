0436dbb5002b396bd1eb9ddf5fe9fe0cc0d47645
FHNora <horvath.nora92@gmail.com>
2020-12-03 21:05:26 +0100

    class Vet modified

--- src/main/java/stringbasic/Vet.java
+++ src/main/java/stringbasic/Vet.java
@@ -7,8 +7,8 @@ public class Vet {
     List<String> pets = new ArrayList<>();
     Pet pet = new Pet("Zorro", 2017,"male", "487521");
 
-    public List<String> add(String pet, List<String> pets){
-        for (String p : pets){
+    public List<Pet> add(Pet pet, List<Pet> pets){
+        for (Pet p : pets){
             if (!areEquals(p, pet)){
                 pets.add(pet);
             }
@@ -16,14 +16,12 @@ public class Vet {
         return pets;
     }
 
-    public boolean areEquals(String pet, String pet2){
-        if (pet){
-            return true;
-        }
-        return false;
+    public boolean areEquals(Pet pet, Pet pet2){
+       return pet.getRegistrationNumber().equals(pet2.getRegistrationNumber());
     }
 
     public List<String> getPets() {
+
         return pets;
     }
 }
--- src/main/java/stringmethods/FileNameManipulator.java
+++ src/main/java/stringmethods/FileNameManipulator.java
@@ -1,4 +1,12 @@
 package stringmethods;
 
 public class FileNameManipulator {
+
+    public char findLastCharacter(String str){
+        return str.charAt(0);
+    }
+
+    public String findFileExtension(String filename){
+        return filename.length();
+    }
 }
