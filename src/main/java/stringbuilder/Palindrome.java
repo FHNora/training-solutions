8c5a717544f3d384a044a3e2216963694dbc4c0f
FHNora <horvath.nora92@gmail.com>
2020-12-05 14:42:01 +0100

    class PalindromeMain added

--- src/main/java/stringbuilder/Palindrome.java
+++ src/main/java/stringbuilder/Palindrome.java
@@ -3,7 +3,7 @@ package stringbuilder;
 public class Palindrome {
 
     public boolean isPalindrome(String word){
-        StringBuilder sb = new StringBuilder();
-        
+        StringBuilder sb = new StringBuilder(word);
+        return sb.equals(sb.reverse());
     }
 }
