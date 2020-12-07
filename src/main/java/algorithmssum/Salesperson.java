3e38b6d958d21e06c42870b54510097196ad4dfb
FHNora <horvath.nora92@gmail.com>
2020-12-07 15:10:03 +0100

    class SalesMain added

--- src/main/java/algorithmssum/Salesperson.java
+++ src/main/java/algorithmssum/Salesperson.java
@@ -1,4 +1,18 @@
 package algorithmssum;
 
+
 public class Salesperson {
+    private String name;
+    private int amount;
+
+    public Salesperson (String name, int amount){
+        this.name = name;
+        this.amount = amount;
+    }
+
+    public int income(int amount){
+        int sum = 0;
+        amount += sum;
+        return sum;
+    }
 }
