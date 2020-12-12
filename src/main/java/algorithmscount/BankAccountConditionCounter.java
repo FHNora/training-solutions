665451a78bf25599ff1f61017af6fc18eaaa0964
FHNora <horvath.nora92@gmail.com>
2020-12-12 11:36:39 +0100

    class BankAccountConditionCounter modified

--- src/main/java/algorithmscount/BankAccountConditionCounter.java
+++ src/main/java/algorithmscount/BankAccountConditionCounter.java
@@ -5,4 +5,11 @@ import java.util.List;
 
 public class BankAccountConditionCounter {
 
+    public static int countAccount()
+
+    public static void main(String[] args) {
+        List<BankAccount> bankAccount = new ArrayList<>();
+        BankAccount bankAccount1 = new BankAccount("John", "1653724180", 100_000);
+
+    }
 }
