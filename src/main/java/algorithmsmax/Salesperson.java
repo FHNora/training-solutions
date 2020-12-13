84699ce6153c9e98119cc00891bca97a0d10a8a3
FHNora <horvath.nora92@gmail.com>
2020-12-13 14:46:59 +0100

    class SalesMain added

--- src/main/java/algorithmsmax/Salesperson.java
+++ src/main/java/algorithmsmax/Salesperson.java
@@ -1,4 +1,14 @@
 package algorithmsmax;
 
 public class Salesperson {
+
+    private String name;
+    private int amount;
+    private int target;
+
+    public Salesperson(String name, int amount, int target){
+        this.name = name;
+        this.amount = amount;
+        this.target = target;
+    }
 }
