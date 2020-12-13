e3ecc3e9521393a9ea1948ef9de2f3f9cd80f222
FHNora <horvath.nora92@gmail.com>
2020-12-13 21:47:40 +0100

    class BankAccount modified

--- src/main/java/algorithmsdecision/BankAccount.java
+++ src/main/java/algorithmsdecision/BankAccount.java
@@ -1,4 +1,30 @@
 package algorithmsdecision;
 
 public class BankAccount {
+
+    private String nameOfOwner;
+    private String accountNumber;
+    private int balance;
+
+    public BankAccount(String nameOfOwner, String accountNumber, int balance) {
+        this.nameOfOwner = nameOfOwner;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getNameOfOwner() {
+        return nameOfOwner;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int withdraw(int balance, int amount){
+        
+    }
 }
