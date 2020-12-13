35cf534c99247a6d9f93c7f1a08b441a36c51d5e
FHNora <horvath.nora92@gmail.com>
2020-12-13 16:15:43 +0100

    class SalesMain modified

--- src/main/java/algorithmsmax/SalesMain.java
+++ src/main/java/algorithmsmax/SalesMain.java
@@ -1,17 +1,41 @@
 package algorithmsmax;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SalesMain {
 
     public static String bestSalesperson(List<Salesperson> salesperson){
         int max = 0;
+        String best = "";
         for (Salesperson bs : salesperson) {
-            String best = bs.getName();
             if (bs.getAmount() > bs.getTarget()) {
                 max = bs.getAmount();
+                best = bs.getName();
             }
         }
-        return ;
+        return best;
+    }
+
+    public static String absolut(List<Salesperson> salesperson){
+        int max = 0;
+        for (Salesperson ab : salesperson) {
+            (ab.getTarget() - ab.getAmount()) == int difference;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        List<Salesperson> salesperson = new ArrayList<>();
+        Salesperson salesperson1 = new Salesperson("John", 100_000, 50_000);
+        Salesperson salesperson2 = new Salesperson("Jack", 100_000,30_000);
+        Salesperson salesperson3 = new Salesperson("Jane", 100_000,10_000);
+
+        salesperson.add(salesperson1);
+        salesperson.add(salesperson2);
+        salesperson.add(salesperson3);
+
+        System.out.println(bestSalesperson(salesperson));
+
     }
 }
