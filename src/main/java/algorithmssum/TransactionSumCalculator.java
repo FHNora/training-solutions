e7e7164f8014ded08ebbd1a3dd03ea71053ebfff
FHNora <horvath.nora92@gmail.com>
2020-12-14 15:47:46 +0100

    class TransactionSumCalculator modified

--- src/main/java/algorithmssum/TransactionSumCalculator.java
+++ src/main/java/algorithmssum/TransactionSumCalculator.java
@@ -1,4 +1,16 @@
 package algorithmssum;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TransactionSumCalculator {
+
+    public static int sumAmountOfCreditEntries(List<Transaction> transactions) {
+
+    }
+
+    public static void main(String[] args) {
+
+        List<Transaction> transactions = new ArrayList<>();
+    }
 }
