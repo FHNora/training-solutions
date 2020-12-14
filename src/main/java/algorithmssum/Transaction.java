8722ca159e663018837a5b40477a6b619ca4d66a
FHNora <horvath.nora92@gmail.com>
2020-12-14 15:45:49 +0100

    class TransactionSumCalculator added

--- src/main/java/algorithmssum/Transaction.java
+++ src/main/java/algorithmssum/Transaction.java
@@ -1,4 +1,12 @@
 package algorithmssum;
 
 public class Transaction {
+    private String accountNumber;
+    private enum transactionOperation{CREDIT, DEBIT};
+    private int amount;
+
+    public Transaction(String accountNumber, int amount) {
+        this.accountNumber = accountNumber;
+        this.amount = amount;
+    }
 }
