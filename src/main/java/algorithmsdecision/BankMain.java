2c5ae5d65e8c857a22dee9da96f7fa205bf986ed
FHNora <horvath.nora92@gmail.com>
2020-12-14 15:39:25 +0100

    class BankMain modified

--- src/main/java/algorithmsdecision/BankMain.java
+++ src/main/java/algorithmsdecision/BankMain.java
@@ -1,4 +1,31 @@
 package algorithmsdecision;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankMain {
+
+    public static boolean isBankAccount(List<BankAccount> bankAccounts, int limit) {
+        for (BankAccount ba : bankAccounts) {
+            if (ba.getBalance() > limit) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        List<BankAccount> bankAccounts = new ArrayList<>();
+        BankAccount bankAccount1 = new BankAccount("John", "1298745", 100_000);
+        BankAccount bankAccount2 = new BankAccount("Jack", "1879525", 200_000);
+        BankAccount bankAccount3 = new BankAccount("Jane", "1658799", 250_000);
+        BankAccount bankAccount4 = new BankAccount("Kate", "1921574", 500_000);
+        bankAccounts.add(bankAccount1);
+        bankAccounts.add(bankAccount2);
+        bankAccounts.add(bankAccount3);
+        bankAccounts.add(bankAccount4);
+        System.out.println(isBankAccount(bankAccounts, 250_000));
+    }
+
 }
