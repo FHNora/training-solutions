f613d00aa9250bab1071db797f4514be3b70abd6
FHNora <horvath.nora92@gmail.com>
2020-12-17 14:50:32 +0100

    class IntegerMaxCalculator modified

--- src/main/java/algorithmsmax/IntegerMaxCalculator.java
+++ src/main/java/algorithmsmax/IntegerMaxCalculator.java
@@ -1,4 +1,32 @@
 package algorithmsmax;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IntegerMaxCalculator {
+
+    public static int maxCalculator(List<Integer> numbers) {
+        int max = 0;
+        for (int num : numbers) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(16);
+        numbers.add(4);
+        numbers.add(0);
+        numbers.add(34);
+        numbers.add(9);
+        numbers.add(48);
+        numbers.add(731);
+        numbers.add(63);
+
+        System.out.println(maxCalculator(numbers));
+    }
 }
