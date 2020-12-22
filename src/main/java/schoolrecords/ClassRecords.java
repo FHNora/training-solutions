6273c25e3541491cb3c82123b677c4633dfc3ba8
FHNora <horvath.nora92@gmail.com>
2020-12-22 15:58:40 +0100

    class ClassRecords modified

--- src/main/java/schoolrecords/ClassRecords.java
+++ src/main/java/schoolrecords/ClassRecords.java
@@ -1,4 +1,24 @@
 package schoolrecords;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class ClassRecords {
+
+    private String ClassName;
+    private Random random;
+    private List<Student> students = new ArrayList<>();
+
+    public boolean addStudent() {
+    }
+
+    public double calculateClassAverage() {
+
+    }
+
+    public double calculateClassAverageBySubject(Subject) {
+
+    }
+
 }
