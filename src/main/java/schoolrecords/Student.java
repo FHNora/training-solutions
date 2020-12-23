7fa08c9221e8ecc8e62741c5361a5ac690187bf3
FHNora <horvath.nora92@gmail.com>
2020-12-23 15:44:25 +0100

    class Student modified

--- src/main/java/schoolrecords/Student.java
+++ src/main/java/schoolrecords/Student.java
@@ -1,4 +1,42 @@
 package schoolrecords;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String name;
+    private List<Mark> marks = new ArrayList<>();
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public double calculateAverage() {
+
+    }
+
+    public double calculateSubjectAverage(Subject subject) {
+
+    }
+
+    public boolean equals(Object) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void grading(Mark mark) {
+
+    }
+
+    private boolean isEmpty(String name) {
+
+    }
+
+    public String toString(){
+        
+    }
+
 }
