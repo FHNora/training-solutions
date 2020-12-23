e292c1a5e6a14da73e0f2b28043e70d3541c7421
FHNora <horvath.nora92@gmail.com>
2020-12-23 15:31:39 +0100

    class Student added

--- src/main/java/schoolrecords/ClassRecords.java
+++ src/main/java/schoolrecords/ClassRecords.java
@@ -6,18 +6,51 @@ import java.util.Random;
 
 public class ClassRecords {
 
-    private String ClassName;
-    private Random random;
+    private String className;
+    private Random rnd;
     private List<Student> students = new ArrayList<>();
 
-    public boolean addStudent() {
+
+    public boolean addStudent(Student student) {
     }
 
     public double calculateClassAverage() {
 
     }
 
-    public double calculateClassAverageBySubject(Subject) {
+    public double calculateClassAverageBySubject(Subject subject) {
+
+    }
+
+    public void ClassRecords(String className, Random rnd) {
+
+    }
+
+    public Student findStudentByName(String name) {
+
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    private boolean isEmpty(String className) {
+
+    }
+
+    private String listStudentNames() {
+
+    }
+
+    public List<StudyResultByName> listStudyResults() {
+
+    }
+
+    public boolean removeStudent(Student student) {
+
+    }
+
+    public Student repetition() {
 
     }
 
