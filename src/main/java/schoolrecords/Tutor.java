96f45d669eff4eb0cab6a14b6844826cc24b4a30
FHNora <horvath.nora92@gmail.com>
2020-12-29 10:53:34 +0100

    class StudyResultByName added

--- src/main/java/schoolrecords/Tutor.java
+++ src/main/java/schoolrecords/Tutor.java
@@ -1,4 +1,24 @@
 package schoolrecords;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tutor {
+
+    private String name;
+    private List<Subject> taughtSubjects = new ArrayList<>();
+
+    public Tutor(String name, List<Subject> taughtSubjects) {
+        this.name = name;
+        this.taughtSubjects = taughtSubjects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean tutorTeachingSubject() {
+
+    }
+
 }
