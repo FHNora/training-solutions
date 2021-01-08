6d320e718f3d898d4e254487d5890ee4cec0f12e
FHNora <horvath.nora92@gmail.com>
2021-01-08 15:53:32 +0100

    class CelestialCoordinates added

--- src/main/java/immutable/Satellite.java
+++ src/main/java/immutable/Satellite.java
@@ -1,4 +1,10 @@
 package immutable;
 
 public class Satellite {
+
+    private Satellite satellite;
+    private final Celestialcordinates destinationCoordinates;
+    private String registerIdent;
+
+    public
 }
