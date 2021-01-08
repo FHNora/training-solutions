18243b201fd18c0c6dcd636dfded16c69cf0421d
FHNora <horvath.nora92@gmail.com>
2021-01-08 15:49:07 +0100

    class Satellite added

---
+++ src/main/java/immutable/SpaceAgency.java
@@ -0,0 +1,14 @@
+package immutable;
+
+public class SpaceAgency {
+
+    private Satellite satellite;
+
+    public void registerSatellite(Satellite satellite) {
+
+    }
+
+    public Satellite findSatelliteByRegisterIdent(String registerIdent) {
+
+    }
+}
--- src/main/java/immutable/Spaceagency.java
+++
@@ -1,4 +0,0 @@
-package immutable;
-
-public class Spaceagency {
-}
