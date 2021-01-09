aad8439c65ccf79d44b421f6db5adff1371aea1b
FHNora <horvath.nora92@gmail.com>
2021-01-09 17:21:32 +0100

    class SpaceAgency modified

--- src/main/java/immutable/SpaceAgency.java
+++ src/main/java/immutable/SpaceAgency.java
@@ -1,14 +1,17 @@
 package immutable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SpaceAgency {
 
-    private Satellite satellite;
+    private List<Satellite> satellites = new ArrayList<>();
 
     public void registerSatellite(Satellite satellite) {
-
+        satellites.add(satellite);
     }
 
     public Satellite findSatelliteByRegisterIdent(String registerIdent) {
-
+        
     }
 }
