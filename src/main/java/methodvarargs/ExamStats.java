548231ec46c2a3e50480f8bed2eb91cac19d73d4
FHNora <horvath.nora92@gmail.com>
2021-01-11 15:45:58 +0100

    class ExamStats modified

--- src/main/java/methodvarargs/ExamStats.java
+++ src/main/java/methodvarargs/ExamStats.java
@@ -1,4 +1,29 @@
 package methodvarargs;
 
 public class ExamStats {
+
+    private int maxPoints;
+
+    public ExamStats(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+
+    public int getNumberOfTopGrades(int limitInPercent, int... results) {
+        int number = 0;
+        for (int r : results) {
+            if (r > limitInPercent) {
+                number++;
+            }
+        }
+        return number;
+    }
+
+    public boolean hasAnyFailed(int limitInPercent, int... results) {
+        for (int r : results) {
+            if (r < limitInPercent) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
