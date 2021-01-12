1fa413bcf0067b77b7b3b2077eb99aa5d3cbf7c7
FHNora <horvath.nora92@gmail.com>
2021-01-12 16:39:40 +0100

    class Robot modified

---
+++ src/main/java/methodchain/Robot.java
@@ -0,0 +1,20 @@
+package methodchain;
+
+public class Robot {
+
+    private int distance;
+    private int azimut;
+
+    public Robot(int distance, int azimut) {
+        this.distance = distance;
+        this.azimut = azimut;
+    }
+
+    public Robot go(int meter) {
+
+    }
+
+    public Robot rotate(int angle) {
+
+    }
+}
