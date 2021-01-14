0ff2d19c6ac93c42d7d29d150e4fe7831d25fe93
FHNora <horvath.nora92@gmail.com>
2021-01-14 16:06:45 +0100

    class BankTransaction modified

--- src/main/java/staticattrmeth/BankTransaction.java
+++ src/main/java/staticattrmeth/BankTransaction.java
@@ -1,4 +1,16 @@
 package staticattrmeth;
 
+
 public class BankTransaction {
+
+    private static int currentMinValue;
+    private static int currentMaxValue;
+
+    public static int getCurrentMinValue() {
+        return currentMinValue;
+    }
+
+    public static int getCurrentMaxValue() {
+        return currentMaxValue;
+    }
 }
